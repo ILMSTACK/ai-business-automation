2b530501f36b20075a3aa9e40a66c8791b392a27
akmalaqim74 <akmalaqim74@gmail.com>
2025-08-17 19:20:09 +0800

    CORS

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ from .routes.business_automation_routes import api as business_automation_api
 def create_app():
     app = Flask(__name__, template_folder="views/templates")
     app.config.from_object(Config)
-    CORS(app, origins="*")
+    CORS(app, resources={r"/*": {"origins": ["*", "http://localhost:4200"], "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"], "allow_headers": "*"}})
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -35,5 +35,12 @@ def create_app():
     # Import and register business automation API namespace
     
     api.add_namespace(business_automation_api, path='/business-automation')
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        return response
 
     return app
